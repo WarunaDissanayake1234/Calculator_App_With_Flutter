c814b3a5d0f90d4b111513491484677d954633ce
Waruna <73792500+WarunaDissanayake1234@users.noreply.github.com>
2021-02-05 23:26:39 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,135 @@ class CalcApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
-      home: Scaffold(
-        backgroundColor: Colors.yellow[50],
-        body: Center(
-          child: CalcButton(
-            text:'1',
-          ),
-        ),
-      ),
-    );
+        title: 'Calculator',
+        home: Scaffold(
+          backgroundColor: Color(0xFF283637),
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CalcButton(
+                      text: 'AC',
+                      fillColor: 0xFF6C807F,
+                    ),
+                    CalcButton(
+                      text: 'C',
+                      fillColor: 0xFF6C807F,
+                    ),
+                    CalcButton(
+                      text: '%',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '/',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CalcButton(
+                      text: '7',
+                    
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '8',
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '9',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '*',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CalcButton(
+                      text: '4',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '5',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '6',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '-',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CalcButton(
+                      text: '1',
+                      
+                      textColor: 0xFF65BDAC
+                    ),
+                    CalcButton(
+                      text: '2',
+                      textColor: 0xFF65BDAC,
+                    
+                    ),
+                    CalcButton(
+                      text: '3',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '+',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CalcButton(
+                      text: '.',
+                      
+                       textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '0',
+                      
+                       textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '00',
+                      
+                      textColor: 0xFF65BDAC,
+                    ),
+                    CalcButton(
+                      text: '=',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                    )
+                  ],
+                ),
+              ]),
+        ));
   }
 }
--- lib/widgets/CalcButton.dart
+++ lib/widgets/CalcButton.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CalcButton extends StatelessWidget {
+  final int textColor;
+  final int fillColor;
+  final double textSize;
   final String text;
-  const CalcButton({Key key, this.text}) : super(key: key);
+  const CalcButton(
+      {Key key,
+      this.fillColor,
+      this.text,
+      this.textColor = 0xFFFFFFFF,
+      this.textSize = 24})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-
-      width: 65,
-      height: 65,
-      child: FlatButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-        child: Text(text),
-        onPressed: () {},
-        color: Colors.blue[200],
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: SizedBox(
+        width: 65,
+        height: 65,
+        child: FlatButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+                textStyle: TextStyle(
+              fontSize: textSize,
+            )),
+          ),
+          onPressed: () {},
+          color: fillColor != null ? Color(fillColor) : null,
+          textColor: Color(textColor),
+        ),
       ),
     );
   }
